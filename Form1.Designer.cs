c55d6cf7d120baad5109c61f9f2ddf16db7a5d04
Lil-fortune <89183750+Lil-fortune@users.noreply.github.com>
2021-08-27 10:10:47 +0200

    changes

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace VaccineReg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ProvinceComboBox = new System.Windows.Forms.ComboBox();
+            this.IDtextBox = new System.Windows.Forms.TextBox();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.FirstnameTextBox = new System.Windows.Forms.TextBox();
+            this.GenderComboBox = new System.Windows.Forms.ComboBox();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vaccine Registration ";
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(54, 98);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(27, 24);
+            this.ID.TabIndex = 1;
+            this.ID.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Surname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(54, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "FIrstname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(54, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(54, 337);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Province";
+            // 
+            // ProvinceComboBox
+            // 
+            this.ProvinceComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProvinceComboBox.FormattingEnabled = true;
+            this.ProvinceComboBox.Items.AddRange(new object[] {
+            "Eastern Cape",
+            "Free State",
+            "Gauteng",
+            "KwaZulu-Natal",
+            "Limpopo",
+            "Mpumalanga",
+            "Northern Cape",
+            "North West",
+            "Western Cape"});
+            this.ProvinceComboBox.Location = new System.Drawing.Point(152, 336);
+            this.ProvinceComboBox.Name = "ProvinceComboBox";
+            this.ProvinceComboBox.Size = new System.Drawing.Size(182, 30);
+            this.ProvinceComboBox.TabIndex = 7;
+            // 
+            // IDtextBox
+            // 
+            this.IDtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDtextBox.Location = new System.Drawing.Point(152, 97);
+            this.IDtextBox.Name = "IDtextBox";
+            this.IDtextBox.Size = new System.Drawing.Size(182, 28);
+            this.IDtextBox.TabIndex = 8;
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SurnameTextBox.Location = new System.Drawing.Point(152, 153);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(182, 28);
+            this.SurnameTextBox.TabIndex = 9;
+            // 
+            // FirstnameTextBox
+            // 
+            this.FirstnameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstnameTextBox.Location = new System.Drawing.Point(152, 217);
+            this.FirstnameTextBox.Name = "FirstnameTextBox";
+            this.FirstnameTextBox.Size = new System.Drawing.Size(182, 28);
+            this.FirstnameTextBox.TabIndex = 10;
+            // 
+            // GenderComboBox
+            // 
+            this.GenderComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenderComboBox.FormattingEnabled = true;
+            this.GenderComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GenderComboBox.Location = new System.Drawing.Point(152, 273);
+            this.GenderComboBox.Name = "GenderComboBox";
+            this.GenderComboBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.GenderComboBox.Size = new System.Drawing.Size(182, 30);
+            this.GenderComboBox.TabIndex = 6;
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterButton.Location = new System.Drawing.Point(133, 388);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(157, 53);
+            this.RegisterButton.TabIndex = 11;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 465);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.FirstnameTextBox);
+            this.Controls.Add(this.SurnameTextBox);
+            this.Controls.Add(this.IDtextBox);
+            this.Controls.Add(this.ProvinceComboBox);
+            this.Controls.Add(this.GenderComboBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox ProvinceComboBox;
+        private System.Windows.Forms.TextBox IDtextBox;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.TextBox FirstnameTextBox;
+        private System.Windows.Forms.ComboBox GenderComboBox;
+        private System.Windows.Forms.Button RegisterButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace VaccineReg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnn = new SqlConnection("Data Source=DESKTOP-8O3T91G;Initial Catalog=VaccineRegistration;Integrated Security=True");
+            String query = "select * from data";
+
+            SqlCommand cmd = new SqlCommand("insert into RegistationDetails(ID, Surname, Firstname, Gender, Province) values(@ID, @Surname, @Firstname, @Gender, @Province)", cnn);
+            cmd.Parameters.AddWithValue("@ID", IDtextBox.Text);
+            cmd.Parameters.AddWithValue("@Surname", SurnameTextBox.Text);
+            cmd.Parameters.AddWithValue("@Firstname", FirstnameTextBox.Text);
+            cmd.Parameters.AddWithValue("@Gender", GenderComboBox.Text);
+            cmd.Parameters.AddWithValue("@Province", ProvinceComboBox.Text);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+    }
+}
